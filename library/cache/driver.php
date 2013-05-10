028bbe0c991b4570dd58d80ce8ef8ef102896e4f
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-05-10 16:12:19 -0400

    Switched from list to set for redis, it's theoretically faster, however due to a bug in phpredis, it's the same. Hopefully the bug will be fixed soon.

--- library/cache/driver.php
+++ library/cache/driver.php
@@ -7,8 +7,8 @@
         public static function increment($key, $pool, $offset=1);
         public static function decrement($key, $pool, $offset=1);
         public static function exists($key, $pool);
-        public static function list_append($key, $pool, $value);
-        public static function list_range($key, $pool, $start=0, $end=-1);
+        public static function list_add($key, $pool, $value);
+        public static function list_get($key, $pool, array $filter=null);
         public static function list_remove($key, $pool, array $remove_keys);
         public static function get($key, $pool);
         public static function get_multi(array $keys, $pool);
--- library/cache/memcache/driver.php
+++ library/cache/memcache/driver.php
@@ -57,7 +57,7 @@
         }
 
         /**
-         * Create or Append a value to the end of a list/array
+         * Create a list and/or Add a value to a list
          *
          * @param string $key
          * @param string $pool
@@ -65,7 +65,33 @@
          *
          * @throws cache_memcache_exception
          */
-        public static function list_append($key, $pool, $value) {
+        public static function list_add($key, $pool, $value) {
+            throw new cache_memcache_exception('List commands are not supported by memcache');
+        }
+
+        /**
+         * Get all members of a list or get matching members of a list
+         *
+         * @param string $key
+         * @param string $pool
+         * @param array  $filter list of keys, an intersection is done
+         *
+         * @throws cache_memcache_exception
+         */
+        public static function list_get($key, $pool, array $filter = null) {
+            throw new cache_memcache_exception('List commands are not supported by memcache');
+        }
+
+        /**
+         * Remove values from a list
+         *
+         * @param string $key
+         * @param string $pool
+         * @param array  $remove_keys
+         *
+         * @throws cache_memcache_exception
+         */
+        public static function list_remove($key, $pool, array $remove_keys) {
             throw new cache_memcache_exception('List commands are not supported by memcache');
         }
 

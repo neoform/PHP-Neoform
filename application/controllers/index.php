3fcea7970603c765734e56a5a65af12f45eb7bf4
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-05-25 12:49:32 -0400

    Removed stuff that doesn't belong here.

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -1,19 +1,7 @@
 <?php
 
+    core::locale()->set_namespace('main');
 
+    $view = new render_view();
 
-    if (core::http()->get_isset('hello')) {
-        $path = core::path('application') . '/data/sample2.wav';
-
-        core::output()
-            ->header("Content-Type: image/png")
-            ->body((string) (new audio_wav($path)));
-    } else {
-
-        core::locale()->set_namespace('main');
-
-        $view = new render_view();
-
-        $view->render('index');
-
-    }
+    $view->render('index');

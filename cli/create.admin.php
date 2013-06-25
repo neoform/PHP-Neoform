32081e45c273a429f3ae245120982f0b9c84c8d9
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-06-24 22:45:12 -0400

    Added CLI script for generating users with all roles. (admin)

---
+++ cli/create.admin.php
@@ -0,0 +1,80 @@
+#!/usr/bin/env php
+<?php
+
+    $root = realpath(__DIR__ . '/..');
+    require_once($root . '/library/core.php');
+    core::init([
+        'extension'   => 'php',
+        'environment' => 'sling',
+
+        'application' => $root . '/application/',
+        'library'     => $root . '/library/',
+        'external'    => $root . '/external/',
+        'entities'    => $root . '/entities/',
+        'logs'        => $root . '/logs/',
+        'website'     => $root . '/www/',
+    ]);
+
+    class create_admin extends cli_model {
+
+        public function init() {
+
+            do {
+                do {
+                    echo self::color_text('Please enter an email address:', 'green', true) . "\n";
+                    $email = self::readline();
+
+                    try {
+                        if (user_api::email_available([ 'email' => $email, ])) {
+                            break;
+                        }
+                    } catch (input_exception $e) {
+                        echo self::color_text($e->email, 'red', true) . "\n";
+                        continue;
+                    }
+
+                    echo self::color_text('That user already exists', 'red', true) . "\n";
+                } while (1);
+
+                echo self::color_text('Enter password', 'green', true) . "\n";
+                $password1 = self::readline();
+
+                echo self::color_text('Repeat password', 'green', true) . "\n";
+                $password2 = self::readline();
+
+                try {
+                    $user = user_api::insert([
+                        'email'     => $email,
+                        'password1' => $password1,
+                        'password2' => $password2,
+                    ]);
+
+                    break;
+
+                } catch (input_exception $e) {
+                    if ($e->errors()) {
+                        foreach ($e->errors() as $k => $v) {
+                            echo self::color_text("{$k}: {$v}", 'red', true) . "\n";
+                        }
+                    }
+                }
+
+            } while (1);
+
+            $roles = new acl_role_collection(null, acl_role_dao::all());
+
+            $user_acl_roles = [];
+            foreach ($roles as $role) {
+                $user_acl_roles[] = [
+                    'user_id'     => $user->id,
+                    'acl_role_id' => $role->id,
+                ];
+            }
+
+            user_acl_role_dao::inserts($user_acl_roles);
+
+            echo self::color_text('Done', 'green', true) . "\n";
+        }
+    }
+
+    new create_admin;

301493552b4a98861d6a4e8079fc2d34924dee3f
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-07-30 23:07:30 -0400

    Added default routes.php and index controller to main library (in case everything is deleted from application/library)

---
+++ library/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * Default routes - only index
+     */
+    class routes implements http_routes {
+        public static function get() {
+            return new http_route([
+                'controller' => 'controller_index',
+            ]);
+        }
+    }

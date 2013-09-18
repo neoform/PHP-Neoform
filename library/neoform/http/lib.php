4fb0f3db34428007e1597150d05fed9d28148076
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-09-17 23:58:38 -0400

    Fixed 404 enforcement function.

--- library/neoform/http/lib.php
+++ library/neoform/http/lib.php
@@ -10,12 +10,17 @@
          * Checks the http segments, if they don't match the $segment_regex then user is redirected to a 404 page
          *
          * @param string $segment_regex
+         *
+         * @return bool true on 404
          */
         public static function limit_url($segment_regex) {
             $segments = http::instance()->segments();
             array_shift($segments); // we don't want to have root be part of this
             if (! preg_match($segment_regex, '/' . join('/', $segments))) {
                 controller::show404();
+                return true;
             }
+
+            return false;
         }
     }

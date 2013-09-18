3c6c7a6f47fcf11a65326a074959e42d07b9745f
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2013-09-18 00:24:56 -0400

    Added allowed GET params to http\lib::limit_url()

--- library/neoform/http/lib.php
+++ library/neoform/http/lib.php
@@ -10,13 +10,20 @@
          * Checks the http segments, if they don't match the $segment_regex then user is redirected to a 404 page
          *
          * @param string $segment_regex
+         * @param array  $permitted_gets GET parameter names that are allowed
          *
          * @return bool true on 404
          */
-        public static function limit_url($segment_regex) {
+        public static function limit_url($segment_regex, array $permitted_gets=[]) {
             $segments = http::instance()->segments();
             array_shift($segments); // we don't want to have root be part of this
             if (! preg_match($segment_regex, '/' . join('/', $segments))) {
+                controller::show404();
+                return true;
+            }
+
+            // If any get values exist that aren't permitted, 404.
+            if (array_diff_key(http::instance()->gets(), array_flip($permitted_gets))) {
                 controller::show404();
                 return true;
             }

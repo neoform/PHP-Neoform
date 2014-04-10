c3d5f8cc51854f2350623d1dcc9c1135dc3aec6e
Ian O'Shaughnessy <ian@oshaughnessy.cc>
2014-04-09 23:55:52 -0400

    Added entity DAO instantiation classes that help create DAO instances that can override the config files, as well as have direct read-only access to the entity's source (thereby alleviating load on the caching engine by not caching rarely used lookups).

--- library/neoform/entity.php
+++ library/neoform/entity.php
@@ -6,6 +6,21 @@
 
         protected static $daos = [];
 
+        protected static $readonly_config = [
+            'source_engine_pool_write'     => null, // set to null, since we can't allow any writing to the source
+
+            'cache_engine'                 => null, // no cache engine is used
+            'cache_engine_pool_read'       => null,
+            'cache_engine_pool_write'      => null,
+
+            'cache_meta_engine'            => null, // no cache engine is used
+            'cache_meta_engine_pool_read'  => null,
+            'cache_meta_engine_pool_write' => null,
+
+            'cache_delete_expire_ttl'      => null,
+
+        ];
+
         /**
          * Get a DAO object based on the config files
          *
@@ -25,5 +40,45 @@
                 }
             }
             return self::$daos[$name];
+        }
+
+        /**
+         * Get a DAO object based on the config files
+         *
+         * This DAO is a separate instance, and is not a singleton, it will be reinstantiated each time this function
+         * is called. Override config data overrides what is found in the config files.
+         *
+         * @param string $name name of the dao
+         * @param array  $override override configurations
+         *
+         * @return entity\record\dao|entity\link\dao
+         */
+        public static function dao_overriden($name, array $override) {
+            if (! isset(self::$daos[$name])) {
+                $class_name = "\\neoform\\{$name}\\dao";
+                $config     = config::instance()['entity'];
+
+                if (isset($config['overrides'][$name])) {
+                    self::$daos[$name] = new $class_name($override + $config['overrides'][$name] + $config['defaults']);
+                } else {
+                    self::$daos[$name] = new $class_name($override + $config['defaults']);
+                }
+            }
+            return self::$daos[$name];
+        }
+
+        /**
+         * Get a read-only DAO object based on the config files
+         *
+         * This object cannot be used for writes, only reads. Attempting to write will cause an error.
+         *
+         * This will directly access the source.
+         *
+         * @param $name
+         *
+         * @return entity\record\dao|entity\link\dao
+         */
+        public static function dao_cacheless($name) {
+            return self::dao_overriden($name, self::$readonly_config);
         }
     }
